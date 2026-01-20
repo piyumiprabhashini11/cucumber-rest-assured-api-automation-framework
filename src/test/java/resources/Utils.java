8085e5cfc33e70a81f583404301cb64e3ffba663
piyumiprabhashini11 <piyumimadduamge1700@gmail.com>
2026-01-20 20:52:17 +0000

    Test results Logging feature (logging.txt) Implementation

--- src/test/java/resources/Utils.java
+++ src/test/java/resources/Utils.java
@@ -1,12 +1,20 @@
 package resources;
 
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.specification.RequestSpecification;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 
 public class Utils {
     RequestSpecification reqSpec;
-    public RequestSpecification requestSpecification(){
-        reqSpec=new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com").addQueryParam("key","qaclick123").build();
+    public RequestSpecification requestSpecification() throws FileNotFoundException {
+        PrintStream log = new PrintStream(new FileOutputStream("logging.txt"));
+        reqSpec=new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com").addQueryParam("key","qaclick123")
+                .addFilter(RequestLoggingFilter.logRequestTo(log))
+                .addFilter(ResponseLoggingFilter.logResponseTo(log)).build();
         return reqSpec;
     }
 }
