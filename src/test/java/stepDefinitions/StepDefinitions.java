5003322c1c3c49ab30298ac5c805d10d8c84c60b
Piyumi Prabhashini <piyumimadduamge1700@gmail.com>
2026-01-20 16:05:05 +0000

    Cucumber rest assured api test framework implementation (#8)
    * Cucumber Junit project structure
    
    * placeValidations.feature file
    
    * feature file update
    
    * .feature file and TestRunner setup
    
    * StepDefinitions.java implementation with gherkin syntax
    
    * Configuration changes

--- src/test/java/stepDefinitions/StepDefinitions.java
+++ src/test/java/stepDefinitions/StepDefinitions.java
@@ -1,6 +1,7 @@
 package stepDefinitions;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
 import static org.testng.Assert.assertEquals;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,60 +10,45 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 import pojo.SetGoogleMapDetails;
 import pojo.Location;
+import resources.TestDataBuild;
+import resources.Utils;
 
-public class StepDefinitions {
+public class StepDefinitions extends Utils {
+	RequestSpecification reqSpec;
 	RequestSpecification req;
 	ResponseSpecification resSpec;
 	Response response;
 	@Given("Add Place Payload")
 	public void add_Place_Payload() {
-		SetGoogleMapDetails ob = new SetGoogleMapDetails();
-		ob.setAccuracy(50);
-		ob.setName("Frontline house");
-		ob.setAddress("29, side layout, cohen 09");
-		ob.setPhone_number("(+91) 983 893 3937");
-		ob.setWebsite("http://google.com");
-		ob.setLanguage("French-IN");
-		
-		List<String> myList = new ArrayList<>();
-		myList.add("shoe park");
-		myList.add("shop");
-		ob.setTypes(myList);
-		
-		Location lc = new Location();
-		lc.setLat(-38.383494);
-		lc.setLng(33.427362);
-		ob.setLocation(lc);
-		
-		RequestSpecification reqSpec=new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com").addQueryParam("key","qaclick123").build();
-		RequestSpecification req=given().spec(reqSpec).body(ob).log().all();
-		
+		TestDataBuild data = new TestDataBuild();
+		req = given().spec(requestSpecification()).body(data.addPlacePayload()).log().all();
+		resSpec = new ResponseSpecBuilder().expectContentType(ContentType.JSON).expectStatusCode(200).build();
+
 	}
 	
 	@When("User calls {string} with POST http request") 
     public void user_calls_with_POST_http_Request(String string){
 //		ResponseSpecification resSpec=new ResponseSpecBuilder().expectStatusCode(200).build();
-		Response response=req.when().post("/maps/api/place/add/json")
+		response=req.when().post("/maps/api/place/add/json")
 				.then().spec(resSpec).extract().response();
-}	
-  
+}
 	@Then("The API call is success with the status code {int}")
 	public void the_API_call_got_success_with_statuscode(Integer int1) {
+
 		assertEquals(response.getStatusCode(),200);
 		}
 	
    @Then("{string} inside the response body is {string}")
    public void inside_the_response_body_is(String key,String expectedValue) {
-	   String resp=response.asString();
-	   JsonPath js = new JsonPath(resp);
+	   JsonPath js = response.jsonPath();
 	   assertEquals(js.getString(key),expectedValue);
-   }	
-
+   }
 }
 
--- src/test/resources/features/placeValidations.feature
+++ src/test/resources/features/placeValidations.feature
@@ -4,6 +4,6 @@ Scenario: Verify if Place is being successfully added using AddPlaceAPI
      Given Add Place Payload
      When User calls "AddPlaceAPI" with POST http request
      Then The API call is success with the status code 200
-     And "Status" inside the response body is "OK"
-     And "Scope" in response body is "API"
+     And "status" inside the response body is "OK"
+     And "scope" inside the response body is "APP"
      
