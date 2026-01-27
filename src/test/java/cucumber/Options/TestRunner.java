686745c25d509cbb209749251ee982ac774dc7e4
Piyumi Prabhashini <piyumimadduamge1700@gmail.com>
2026-01-27 13:36:59 +0000

    Cucumber rest assured api test framework implementation (#27)
    * Cucumber Junit project structure
    
    * placeValidations.feature file
    
    * feature file update
    
    * .feature file and TestRunner setup
    
    * StepDefinitions.java implementation with gherkin syntax
    
    * Configuration changes
    
    * Test results Logging feature (logging.txt) Implementation
    
    * Implementation of utility and global property features
    
    * Implementation of data driven testing mechanism
    
    * Implementation of data driven testing mechanism
    
    * Parameterising API tests with multiple datasets
    
    * Enum class methods implementation
    
    * End to end framework development
    
    * Delete Place Scenario Implementation
    
    * Implementation of tagging mechanism and cucumber Hooks
    
    * Latest test framework changes
    
    * Cucumber HTML Reporting

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -5,7 +5,13 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="classpath:features",glue= {"stepDefinitions"})
+@CucumberOptions(features="classpath:features",glue= {"stepDefinitions"},
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports/cucumber.html"
+        },
+        monochrome = true
+)
 //@CucumberOptions(features="classpath:features",glue= {"stepDefinitions"},tags = "@DeletePlace")
 public class TestRunner {
 }
